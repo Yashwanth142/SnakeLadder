0c338086b6127ee27a1b6437863daafa031a6f3a
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-28 15:53:40 +0530

    Start Postion added

---
+++ SnakeandLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public class SnakeandLadder
+    {
+        public static void SnakeAndLadder() 
+        {
+            int startPosition = 0;
+            Console.WriteLine("Snake and Ladder game played with single player at start position : " + startPosition);
+        }
+    }
+}
